1f9fdb445ed7eefe4d1c25ee0f792a9fc04af758
Ishan Bansal <github.hesitancy@alias.iy4vet.me>
2025-10-05 00:38:55 +0530

    upload answer

---
+++ code.py
@@ -0,0 +1,12 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(3, 'q')
+creg_c = ClassicalRegister(4, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[0])
+circuit.ccx(qreg_q[0], qreg_q[1], qreg_q[2])
+circuit.x(qreg_q[0])
+circuit.z(qreg_q[1])
+circuit.cx(qreg_q[0], qreg_q[1])
